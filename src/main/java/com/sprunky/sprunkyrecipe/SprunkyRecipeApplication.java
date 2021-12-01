dbc365c13cab9c9a7cd74266076c1a363e8eb63c
jonathan.garrido <jonathan.garrido@incentro.com>
2021-12-01 13:17:20 +0100

    Initial commit

---
+++ src/main/java/com/sprunky/sprunkyrecipe/SprunkyRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.sprunky.sprunkyrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprunkyRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprunkyRecipeApplication.class, args);
+    }
+
+}
